d797850fc7f96f93e09a323fb6225555b830042c
Janis Lee <janislee@Janiss-MacBook-Pro.local>
2018-01-27 20:03:01 -0500

    merging

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import { Font, AppLoading, Asset } from 'expo';
 import { Image, Alert, Button, AsyncStorage, Platform, StyleSheet, Text, View } from 'react-native';
-//import { AsyncStorage, Platform, StyleSheet, Text, View, TextInput } from 'react-native';
 import { TabNavigator } from 'react-navigation';
-
-//import SearchHandler from './SearchHandler';
 import SearchScreen from './searchscreen';
-
 
 
 class App extends Component {
@@ -43,23 +39,14 @@ const style = StyleSheet.create({
 
 const HomeScreen = () => (
   <View style={style.pretty}>
-    <Text>Buy Bulk Buddy
-    </Text>
+    <Text>
+    Buy Bulk Buddy</Text>
     <View>
-
+      <Image
+        source={require('./my-icon.png')}
+        style={{width: 300, height: 250}}
+      />
     </View>
-  </View>
-);
-
-const HelpScreen = () => (
-  <View style={style.pretty}>
-    <Text>What can your Bulk Buddy order for you today?</Text>
-    <Button
-      onPress={() => {
-      Alert.alert('Try again buddy!');
-      }}
-    title="Search"
-    />
   </View>
 );
 
